da6ee170ff5756ed2c1a88eb90880fbb4556f7c2
Afrim Kamberi <afrimkamberi2@gmail.com>
2025-01-21 19:32:29 +0100

    auth

--- CarRental/CarRental.DAL/Startup.cs
+++ CarRental/CarRental.DAL/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using CarRental.DAL.Persistence;
+using CarRental.DAL.Persistence.Entities;
 using CarRental.DAL.Persistence.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
 
 namespace CarRental.DAL;
 
@@ -10,10 +13,15 @@ public static class Startup
 {
     public static void RegisterDALServices(this IServiceCollection services, IConfiguration config)
     {
-        services.AddDbContext<CarRentalDbContext>(opt=>
+        services.AddDbContext<CarRentalDbContext>(opt =>
         {
             opt.UseSqlServer(config.GetConnectionString("CarRental"));
         });
+
+        services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<CarRentalDbContext>();
+
         services.AddScoped<ICarBrandsRepository, CarBrandsRepository>();
     }
 }
