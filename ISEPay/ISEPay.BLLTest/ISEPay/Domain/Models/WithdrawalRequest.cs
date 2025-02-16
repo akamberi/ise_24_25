25f5fbef4626b11abb1e448728090b00862182fd
Suada Elezaj <162741384+Suadae@users.noreply.github.com>
2025-02-16 13:25:33 +0100

    TEST

--- ISEPay/ISEPay.BLLTest/ISEPay/Domain/Models/WithdrawalRequest.cs
+++ ISEPay/ISEPay.BLLTest/ISEPay/Domain/Models/WithdrawalRequest.cs
@@ -2,7 +2,7 @@ namespace ISEPay.BLL.ISEPay.Domain.Models
 {
     public class WithdrawalRequest
     {
-        public Guid AccountNumber { get; set; }
+        public string AccountNumber { get; set; }
         public decimal Amount { get; set; }
         public Guid AgentId { get; set; }
         
